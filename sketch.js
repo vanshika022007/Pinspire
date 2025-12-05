bd3a761d5e9baf86ec122be6934ecbed4d5a54b3
vanshika022007 <vanshikamittal20072007@gmail.com>
2025-12-06 03:38:58 +0530

    sketch.js

--- sketch.js
+++ sketch.js
@@ -3,3 +3,12 @@ const modeBtn = document.getElementById("mode");
 modeBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark");
 });
+
+const likeBtns = document.querySelectorAll(".likeBtn");
+
+likeBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        let count = btn.querySelector(".count");
+        count.innerText = Number(count.innerText) + 1;
+    });
+});
